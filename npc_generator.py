151165482291bc7120abb30754d4a10d3efd05d1
Jonathan Hansen <hansen-jonathan@hotmail.com>
2021-11-05 17:56:36 +0100

    Add dependency mechanisism, orientation

--- npc_generator.py
+++ npc_generator.py
@@ -1,4 +1,14 @@
-from src import main
+import json
+import sys
+
+from PySide2 import QtWidgets
+
+from src.main_window import MainWindow
 
 if __name__ == '__main__':
-    main.main()
+    with open("./settings.json", encoding='utf-8') as json_data_file:
+        config = json.load(json_data_file)
+    app = QtWidgets.QApplication(sys.argv)
+    pnj_generator = MainWindow(config)
+    pnj_generator.show()
+    sys.exit(app.exec_())
--- src/main_window.py
+++ src/main_window.py
@@ -12,20 +12,23 @@ class MainWindow(QtWidgets.QWidget):
         super(MainWindow, self).__init__(parent=parent)
         self.generated_values = {}
         self.data_loader = DataLoader(settings)
-        self.npc = Npc.create_random(self.data_loader)
+        self.npc = None
 
         self.setWindowTitle('NPyC Generator')
         self.resize(800, 600)
 
-        self.physical_widget = QtWidgets.QLabel(self.npc.get_physical_description())
+        self.physical_widget = QtWidgets.QLabel()
         self.physical_widget.setWordWrap(True)
-        self.character_widget = QtWidgets.QLabel(self.npc.get_character_description())
+        self.character_widget = QtWidgets.QLabel()
         self.character_widget.setWordWrap(True)
-        self.plothook_widget = QtWidgets.QLabel(self.npc.get_plot_hook())
+        self.plothook_widget = QtWidgets.QLabel()
         self.plothook_widget.setWordWrap(True)
+        self.relationships_widget = QtWidgets.QLabel()
+        self.relationships_widget.setWordWrap(True)
         self.generate_btn = QtWidgets.QPushButton('Generate (㇏(•̀ᵥᵥ•́)ノ)')
         self.export_btn = QtWidgets.QPushButton('Export')
 
+        self.generate()
         self._set_layout()
         self._connection()
 
@@ -52,10 +55,17 @@ class MainWindow(QtWidgets.QWidget):
         plothook_grp_cont_widget.setLayout(plothook_grp_cont_layout)
         plothook_grp_cont_layout.addWidget(self.plothook_widget)
 
+        # Relationships
+        relationships_grp_cont_widget = QtWidgets.QGroupBox('Relationships')
+        relationships_grp_cont_layout = QtWidgets.QVBoxLayout()
+        relationships_grp_cont_widget.setLayout(relationships_grp_cont_layout)
+        relationships_grp_cont_layout.addWidget(self.relationships_widget)
+
         hor_layout1.addWidget(physical_grp_cont_widget)
         hor_layout1.addWidget(character_grp_cont_widget)
 
         hor_layout2.addWidget(plothook_grp_cont_widget)
+        hor_layout2.addWidget(relationships_grp_cont_widget)
 
         main_layout.addLayout(hor_layout1)
         main_layout.addLayout(hor_layout2)
@@ -72,21 +82,10 @@ class MainWindow(QtWidgets.QWidget):
         self.physical_widget.setText(self.npc.get_physical_description())
         self.character_widget.setText(self.npc.get_character_description())
         self.plothook_widget.setText(self.npc.get_plot_hook())
+        self.relationships_widget.setText(self.npc.get_relationships())
 
     def export(self):
         filename = QtWidgets.QFileDialog.getSaveFileName(self, "Save file", "", ".json")
         filename = "%s%s" % (filename[0], filename[1])
         with open(filename, 'w',  encoding='utf-8') as outfile:
             json.dump(self.generated_values, outfile, ensure_ascii=False, indent=2)
-
-
-def main():
-    with open("./settings.json", encoding='utf-8') as json_data_file:
-        config = json.load(json_data_file)
-    app = QtWidgets.QApplication(sys.argv)
-    pnj_generator = MainWindow(config)
-    pnj_generator.show()
-    sys.exit(app.exec_())
-
-
-
